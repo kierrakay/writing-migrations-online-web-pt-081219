241e7a5e18e243059dba7723290d42f64e7b68c0
Kierra M <k.merriman32@hotmail.com>
2019-11-03 16:10:01 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -11,4 +11,4 @@ end
 #   up     001             Create students
 #   up     002             Add grade and birthdate to students
 
-# when you run 
+# when you run rake db:migrate:status and the status is down it means it didnt successfully go through but when the status says up it means it worked. you cant add more than one column on one line thats why its set up that way #4-5
